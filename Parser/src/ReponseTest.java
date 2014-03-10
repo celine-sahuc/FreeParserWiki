2bd740c81170a81f38c3abbf4c691b648cae2646
HeisenbergMeth <eyesun@free.fr>
2014-03-10 11:25:41 +0100

    Voici la classe Réponse ainsi qu'une partie des tests associés.

---
+++ Parser/src/ReponseTest.java
@@ -0,0 +1,69 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author 20900325
+ *
+ */
+public class ReponseTest {
+	protected Reponse op;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		op = new Reponse();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetReponse() {
+		Reponse testObject = new Reponse();
+		testObject.setReponse("Un animal");
+		assertEquals("Un animal", testObject.getReponse());
+	}
+
+	@Test
+	public void testSetFeedback() {
+		Reponse testObject = new Reponse();
+		testObject.setFeedback("Je me suis trompé");
+		assertEquals("Je me suis trompé", testObject.getFeedback());
+	}
+	
+	@Test
+	public void testSetCorrect() {
+		Reponse testObject = new Reponse();
+		testObject.setCorrect(true);
+		assertEquals(true, testObject.isCorrect());
+	}
+}
