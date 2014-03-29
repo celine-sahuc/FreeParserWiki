0ae3052c7cee4e645d333d9ca1a6f88cf7405d45
Céline Sahuc <celine.sahuc@gmail.com>
2014-03-29 14:23:40 +0100

    #8 #6 tests parser 100% de couverture de code

--- src/test/java/com/mycompany/parserceline/ParserTest.java
+++ src/test/java/com/mycompany/parserceline/ParserTest.java
@@ -1,18 +1,14 @@
 /**
- * 
+ *
  */
 package com.mycompany.parserceline;
 
 import java.util.ArrayList;
 import mycompany.parserceline.Parser;
-import mycompany.parserceline.Parser;
 import static org.junit.Assert.*;
 
 import mycompany.parserceline.Question;
-import mycompany.parserceline.Question;
 import mycompany.parserceline.Reponse;
-import mycompany.parserceline.Reponse;
-import mycompany.parserceline.Type;
 import mycompany.parserceline.Type;
 
 import org.junit.After;
@@ -27,81 +23,102 @@ import org.junit.Test;
  */
 public class ParserTest {
 
-	private Parser p;
-	private Question q;
-	private ArrayList<Reponse> r; 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    private Parser p;
+    private Question q;
+    private ArrayList<Reponse> r;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        p = new Parser();
+        r = new ArrayList<Reponse>();
+    }
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
+    @Test
+    public void testTypeSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        assertEquals(q.getType(), Type.SINGLE);
+    }
 
-		p = new Parser();
-		String question = "{Comment �a va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
-		q = p.parserFonction(question);
-		r = new ArrayList<Reponse>();
-	}
+    @Test
+    public void testQuestion() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        assertEquals(q.getTexteQuestion(), "Comment ça va? ");
+    }
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
+    @Test
+    public void testSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        assertEquals(q.getNbReponsesCorrects(), 1);
+    }
 
-	@Test
-	public void testType() {
-		assertEquals(q.getType(), Type.SINGLE ); 
-	}
+    @Test
+    public void testReponseSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).getReponse(), " + The correct answer");
+        assertEquals(r.get(1).getReponse(), "- Distractor");
+        assertEquals(r.get(2).getReponse(), "- Distractor");
+    }
 
-	
-	@Test
-	public void testQuestion() {
-		
-		assertEquals(q.getTexteQuestion(), "Comment �a va? "); 
-	}
-@Test
-public void testNbReponse() {
-	
-	assertEquals(q.getNbReponsesCorrects(), 1); 
-}
-@Test
-public void testReponse() {
-	r = q.getListeReponses();
-		assertEquals(r.get(0).getReponse(), " + The correct answer");
-		assertEquals(r.get(1).getReponse(), "- Distractor");
-		assertEquals(r.get(2).getReponse(), "- Distractor");
+    @Test
+    public void testReponseCorrecteSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).isCorrect(), true);
+        assertEquals(r.get(1).isCorrect(), false);
+        assertEquals(r.get(2).isCorrect(), false);
+    }
 
-}
+    @Test
+    public void testTypeMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        assertEquals(q.getType(), Type.MULTIPLE);
+    }
 
+    @Test
+    public void testQuestionMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        assertEquals(q.getTexteQuestion(), "Comment ça va? ");
+    }
 
-public void testReponseCorrecte() {
-	r = q.getListeReponses();
-		assertEquals(r.get(0).isCorrect(), true);
-		assertEquals(r.get(1).isCorrect(), false);
-		assertEquals(r.get(2).isCorrect(), false);
-}
+    @Test
+    public void testNbReponseMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
 
+        assertEquals(q.getNbReponsesCorrects(), 2);
+    }
 
-public void testReponseFeedback() {
-	r = q.getListeReponses();
-		assertEquals(r.get(0).getFeedback(), null);
-		assertEquals(r.get(1).getFeedback(), null);
-		assertEquals(r.get(2).getFeedback(), null);
-}
+    @Test
+    public void testReponseMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).getReponse(), " + Correct answer");
+        assertEquals(r.get(1).getReponse(), "- Incorrect answer");
+        assertEquals(r.get(2).getReponse(), "+ Correct answer");
+        assertEquals(r.get(3).getReponse(), "- Incorrect answer");
+    }
+
+    @Test
+    public void testReponseCorrecteMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).isCorrect(), true);
+        assertEquals(r.get(1).isCorrect(), false);
+        assertEquals(r.get(2).isCorrect(), true);
+        assertEquals(r.get(3).isCorrect(), false);
+    }
 
 }
