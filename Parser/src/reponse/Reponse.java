c2c3d3760e7f81b412f1728a43157b11099ed1fc
Céline Sahuc <celine.sahuc@gmail.com>
2014-04-05 19:15:10 +0200

    suppression dossier src et ajout d'un constructeur dans Réponse.java

--- Parser/src/reponse/Reponse.java
+++ Parser/src/reponse/Reponse.java
@@ -1,26 +1,51 @@
-package reponse;
+package mycompany.parserceline;
 
 public class Reponse {
 	private String reponse;
 	private boolean correct;
 	private String feedback;
-	
-	public String getReponse() {
-		return reponse;
+
+
+	public Reponse(String r, boolean c, String f) {
+		this.reponse = r;
+		this.correct = c;
+		this.feedback = f;
+		
 	}
-	public void setReponse(String reponse) {
+	
+	public Reponse(String r, boolean c) {
+		this.reponse = r;
+		this.correct = c;
+		this.feedback = null;
+		
+	}
+	
+	public final String getReponse() {
+		return this.reponse;
+	}
+
+	public final void setReponse(String reponse) {
 		this.reponse = reponse;
 	}
-	public String getFeedback() {
+	public final String getFeedback() {
 		return feedback;
 	}
-	public void setFeedback(String feedback) {
+	public final void setFeedback(String feedback) {
 		this.feedback = feedback;
 	}
-	public boolean isCorrect() {
+	public final boolean isCorrect() {
 		return correct;
 	}
-	public void setCorrect(boolean correct) {
-		this.correct = correct;
-	}
-} 
+	public final void setCorrect(boolean co) {
+		this.correct = co;
+	} 
+	
+	public final void afficher() {
+      	 System.out.println(this.reponse);
+       	 System.out.println(this.correct);
+       	 System.out.println(this.feedback);
+
+	
+}
+
+}
---
+++ Parser/src/tests/ParserTest.java
@@ -0,0 +1,121 @@
+/**
+ *
+ */
+package com.mycompany.parserceline;
+
+import java.util.ArrayList;
+import mycompany.parserceline.Parser;
+import static org.junit.Assert.*;
+
+import mycompany.parserceline.Question;
+import mycompany.parserceline.Reponse;
+import mycompany.parserceline.Type;
+import org.junit.Before;
+
+import org.junit.Test;
+
+/**
+ * @author 21003139
+ *
+ */
+public class ParserTest {
+
+    private Parser p;
+    private Question q;
+    private ArrayList<Reponse> r;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        p = new Parser();
+        r = new ArrayList<Reponse>();
+    }
+
+    @Test
+    public void testTypeSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        assertEquals(q.getType(), Type.SINGLE);
+    }
+
+    @Test
+    public void testQuestion() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        assertEquals(q.getTexteQuestion(), "Comment ça va? ");
+    }
+
+    @Test
+    public void testSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        assertEquals(q.getNbReponsesCorrects(), 1);
+    }
+
+    @Test
+    public void testReponseSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).getReponse(), " + The correct answer");
+        assertEquals(r.get(1).getReponse(), "- Distractor");
+        assertEquals(r.get(2).getReponse(), "- Distractor");
+    }
+
+    @Test
+    public void testReponseCorrecteSingle() {
+        String question = "{Comment ça va? |type=\"()\"} + The correct answer.- Distractor.- Distractor. - Distractor.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).isCorrect(), true);
+        assertEquals(r.get(1).isCorrect(), false);
+        assertEquals(r.get(2).isCorrect(), false);
+    }
+
+    @Test
+    public void testTypeMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        assertEquals(q.getType(), Type.MULTIPLE);
+    }
+
+    @Test
+    public void testQuestionMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        assertEquals(q.getTexteQuestion(), "Comment ça va? ");
+    }
+
+    @Test
+    public void testNbReponseMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+
+        assertEquals(q.getNbReponsesCorrects(), 2);
+    }
+
+    @Test
+    public void testReponseMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).getReponse(), " + Correct answer");
+        assertEquals(r.get(1).getReponse(), "- Incorrect answer");
+        assertEquals(r.get(2).getReponse(), "+ Correct answer");
+        assertEquals(r.get(3).getReponse(), "- Incorrect answer");
+    }
+
+    @Test
+    public void testReponseCorrecteMultiple() {
+        String question = "{Comment ça va? |type=\"[]\"} + Correct answer.- Incorrect answer.+ Correct answer.- Incorrect answer.";
+        q = p.parserFonction(question);
+        r = q.getListeReponses();
+        assertEquals(r.get(0).isCorrect(), true);
+        assertEquals(r.get(1).isCorrect(), false);
+        assertEquals(r.get(2).isCorrect(), true);
+        assertEquals(r.get(3).isCorrect(), false);
+    }
+
+}
